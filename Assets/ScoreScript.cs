ad2cda6014e74aade1168e7c1cd06e2b7ff267b9
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-19 14:42:15 +0900

    playerprefs

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour {
+	GameObject score;
+	Text text;
+	// Use this for initialization
+	void Start () {
+		var i =PlayerPrefs.GetInt("highscore",1);
+		score = GameObject.Find ("SCORE");
+		text = score.GetComponent<Text> ();
+		text.text = "SCORE:"+(i.ToString ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
